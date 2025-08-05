d4143566dd5c495e7a4d6e3c4575f6f9a8616a58
hung-senbox <mr.hung@senbox.org>
2025-08-05 11:44:17 +0700

    update

--- internal/user/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type User struct {
-	UserID   string `json:"user_id"`
-	UserName string `json:"user_name"`
-	FullName string `json:"full_name"`
-	Role     string `json:"role"`
-	Avartar  string `json:"avatar"`
-}
